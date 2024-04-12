9ee2bc779216e084853222b1e514a1cf6322de0e
sekharjpapp <43300292+sekharjpapp@users.noreply.github.com>
2024-04-12 17:53:55 +0530

    bugfix

--- src/main/java/com/pixel/controller/ResourceController.java
+++ src/main/java/com/pixel/controller/ResourceController.java
@@ -10,4 +10,7 @@ public class ResourceController {
     public String getIndex() {
         return "Get Index";
     }
+    public void bugFix() {
+        System.out.println("Bug Fix in main");
+    }
 }
